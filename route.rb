9287d4f87e7dcbb87eccbb356aa9266b0dba4d7d
KALi <aleksandr-slap@rambler.ru>
2024-10-22 21:07:31 +0300

    aded class route + corrections

---
+++ route.rb
@@ -0,0 +1,26 @@
+require_relative 'Station.rb'
+
+class Route
+  attr_reader :first_station, :last_station, :intermediate_stations
+
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @intermediate_stations = []
+  end
+
+  def add_intermedian_station(name_station)
+    puts 'Such a station already exists' if intermediate_stations.include?(name_station)
+    intermediate_stations << name_station
+    puts "Stantion #{name_station.name} adeed"
+  end
+
+  def delete_station(name_station)
+    puts 'There is no such station' if @intermediate_stations.include(name_station)
+    intermediate_stations.delete(name_station)
+  end
+
+  def show_stations
+    [first_station] + intermediate_stations + [last_station]
+  end
+end
---
+++ train.rb
@@ -0,0 +1,9 @@
+class Train
+
+  def initialize(number, type, vagon)
+    @number = number
+    @type = type
+    @vagon = vagon
+    @speed = 0
+  end  
+end  
