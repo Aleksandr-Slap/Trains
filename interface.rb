1e5e27262781118f20ac97a8c9b5ebb7a9e85677
KALi <aleksandr-slap@rambler.ru>
2024-10-24 21:16:23 +0300

    added methods in trains.rb

--- interface.rb
+++ interface.rb
@@ -4,9 +4,16 @@ require_relative 'Train.rb'
 
 station = Station.new('Volhov')
 station2 = Station.new('SpB')
+station3 = Station.new('MGA')
 route1 = Route.new(station, station2)
+route1.add_intermedian_station(station3)
 train = Train.new(123, 'Cargo', 10)
 train.assign_a_route(route1)
 train.next_station
+train.next_station
 train.previous_station
+train.show_current_station
+train.show_next_station
+train.show_previous_station
 # train.join_vagon
+# binding.irb
--- train.rb
+++ train.rb
@@ -9,14 +9,6 @@ class Train
     @speed = 0
   end
 
-  def join_vagon
-    speed == 0 ? self.vagons += 1 : "The train is moving!"
-  end
-
-  def unhook_vagon
-    speed == 0 ? self.vagons -= 1 : "The train is moving!"
-  end
-
   def assign_a_route(route)
     @route = route.show_stations
     self.route[0].accept_train(self)
@@ -24,17 +16,38 @@ class Train
   end
 
   def next_station
-    return puts "This last station" if self.route.last == current_station
+    return puts 'This last station' if self.route.last == current_station
     current_station.send_a_train(self)
     @current_station = self.route[self.route.find_index(current_station) + 1 ]
     current_station.accept_train(self)
   end
 
   def previous_station
-    # binding.irb
-    return puts "This first station" if self.route.first == current_station
+    return puts 'This first station' if self.route.first == current_station
     current_station.send_a_train(self)
     @current_station = self.route[self.route.find_index(current_station) - 1 ]
     current_station.accept_train(self)
+  end
+
+  def show_current_station
+    puts "Curent station is #{@current_station.name}"
+  end
+
+  def show_next_station 
+    return puts 'This last station' if @current_station == @route.last
+    puts "Next station is #{@route[@route.find_index(current_station) + 1 ].name }"
+  end
+  
+  def show_previous_station
+    return puts 'This first station' if @current_station == @route.first
+    puts "Previous station is #{@route[@route.find_index(current_station) - 1 ].name }"
   end  
+
+  def join_vagon
+    speed == 0 ? self.vagons += 1 : 'The train is moving!'
+  end
+
+  def unhook_vagon
+    speed == 0 ? self.vagons -= 1 : 'The train is moving!'
+  end
 end
