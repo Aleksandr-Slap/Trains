03527a5fb66d964ddd954301f73a4a60441fe532
KALi <aleksandr-slap@rambler.ru>
2024-10-28 21:53:17 +0300

    foo

--- interface.rb
+++ interface.rb
@@ -1,19 +1,41 @@
-require_relative 'Station.rb'
-require_relative 'Route.rb'
-require_relative 'Train.rb'
+require_relative 'station.rb'
+require_relative 'route.rb'
+require_relative 'train.rb'
+require_relative 'passenger_train'
+require_relative 'cargo_train'
+require_relative 'vagon'
+require_relative 'passenger_vagon.rb'
+require_relative 'cargo_vagon.rb'
 
+# cargo1 = PassengerVagon.new
+# cargo2 = CargoVagon.new
+# puts cargo1.type
+# puts cargo2.type
 station = Station.new('Volhov')
 station2 = Station.new('SpB')
 station3 = Station.new('MGA')
 route1 = Route.new(station, station2)
 route1.add_intermedian_station(station3)
-train = Train.new(123, 'Cargo', 10)
-train.assign_a_route(route1)
-train.next_station
-train.next_station
-train.previous_station
-train.show_current_station
-train.show_next_station
-train.show_previous_station
-# train.join_vagon
+trainP = PassengerTrain.new(123)
+trainC = CargoTrain.new(456)
+# puts trainP.type
+# puts trainC.type
+# trainP.assign_a_route(route1)
+# trainP.next_station
+# trainP.next_station
+# trainP.previous_station
+# trainP.show_current_station
+# trainP.show_next_station
+# trainP.show_previous_station
+vagon1 = PassengerVagon.new
+vagon2 = PassengerVagon.new
+vagon3 = PassengerVagon.new
+vagon4 = CargoVagon.new
+trainP.join_vagon(vagon1)
+trainP.join_vagon(vagon2)
+trainP.join_vagon(vagon3)
+puts trainP.join_vagon(vagon4)
+puts trainP.vagons.count
+trainP.unhook_vagon(vagon3)
 # binding.irbe
+puts trainP.vagons.count
